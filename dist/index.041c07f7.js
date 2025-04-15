c5289f06d867adaf83d176185a38086699c97873
umavanmoriel <umeda19971102@gmail.com>
2025-04-15 19:41:57 +0200

    Lade till function för cirkeldiagram

--- dist/index.041c07f7.js
+++ dist/index.041c07f7.js
@@ -626,9 +626,13 @@ async function processData() {
         const result = await getCoursesInfo();
         console.log('Received data:', result);
         const filterCourses = result.filter((item)=>item.type === "Kurs");
-        let newArray = filterCourses.sort((a, b)=>b.applicantsTotal - a.applicantsTotal).slice(0, 6);
-        createChart(newArray);
-        console.log(newArray);
+        let coursesArray = filterCourses.sort((a, b)=>b.applicantsTotal - a.applicantsTotal).slice(0, 6);
+        const filterProgram = result.filter((item)=>item.type === "Program");
+        let programArray = filterProgram.sort((a, b)=>b.applicantsTotal - a.applicantsTotal).slice(0, 6);
+        createChart(coursesArray);
+        console.log(coursesArray);
+        createChartPie(programArray);
+        console.log(programArray);
     } catch (error) {
         console.error('Error processing data:', error);
     }
@@ -638,6 +642,30 @@ function createChart(data) {
     const ctx = document.getElementById('diagram').getContext('2d');
     new Chart(ctx, {
         type: 'bar',
+        data: {
+            labels: data.map((row)=>row.name),
+            datasets: [
+                {
+                    label: 'Acquisitions by year',
+                    data: data.map((row)=>row.applicantsTotal)
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+function createChartPie(data) {
+    // Se till att elementet med ID 'diagram' finns i DOM
+    const ctx = document.getElementById('piediagram').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie',
         data: {
             labels: data.map((row)=>row.name),
             datasets: [
